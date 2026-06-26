3f513431c33337b98fb831eb2e406be2cedd65c4
etenna-maker <elnathancode@gmail.com>
2026-06-26 12:47:58 -0700

    Add task 6: concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print("Welcome to " + str1 + " " + str2 + "!")
+str1 = "object-oriented programming"
+str2 = "Python"
+print("Welcome to " + str1[19:27] + str2[2:6] + "!")
