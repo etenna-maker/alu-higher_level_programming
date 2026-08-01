120f41c08a4002a4bb6d49ceb03c1483eeecbcd6
etenna-maker <e.tenna@alustudent.com>
2026-08-01 15:05:03 -0700

    Fix 0-body_size.sh to follow redirects with -L

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size, in bytes, of the body of the response from a URL
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
+curl -s -L -o /dev/null -w "%{size_download}\n" "$1"
