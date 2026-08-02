3821c37b371c15b7ea8054e5dd4a1051bd9d108d
etenna-maker <e.tenna@alustudent.com>
2026-08-02 02:25:52 -0700

    Add python-network_0 tasks 0-5 with redirect handling

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the response body only when the HTTP status code is 200
-[ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" = "200" ] && curl -s "$1"
+# Displays the response body only when the final HTTP status code is 200
+[ "$(curl -s -L -o /dev/null -w '%{http_code}' "$1")" = "200" ] && curl -s -L "$1"
